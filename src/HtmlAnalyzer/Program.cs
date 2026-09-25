a75d9f5c1638a55bc205482e074948769ae59032
zynres <killp8918@gmail.com>
2026-09-25 16:52:19 +0300

    feat: add database context

--- src/HtmlAnalyzer/Program.cs
+++ src/HtmlAnalyzer/Program.cs
@@ -1,9 +1,15 @@
+using HtmlAnalyzer.Services.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataBaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Db")));
 
 var app = builder.Build();
 
@@ -13,8 +19,13 @@ if (app.Environment.IsDevelopment())
     app.MapSwaggerUI(setupAction: setup =>
     {
         setup.RoutePrefix = "api/swagger";
-        setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Analyzer V1");
+        setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Analyzer");
     });
+
+    using var scope = app.Services.CreateScope();
+
+    var context = scope.ServiceProvider.GetRequiredService<DataBaseContext>();
+    context.Database.Migrate();
 }
 else
 {
